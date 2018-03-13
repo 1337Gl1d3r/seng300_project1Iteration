8cee78ccf0267a1b1982d20b7bb81ef0e9c0cc16
jessicajp <37085601+jessicajp@users.noreply.github.com>
2018-03-13 04:01:34 -0600

    Jessica's work space; set up of tests
    I've created a base for the tests, for each method in our scanner I've made a test case, and then a test suite with all these test cases.
    Currently no actual tests. ¯\_(ツ)_/¯
    Scanner has no changes made to it.
    I hope this is ok, keeping everything together like this made the most sense to me.

---
+++ SENG Group Iteration 1/src/seng_project_iteration_1/convertToStringTest.java
@@ -0,0 +1,15 @@
+package seng_project_iteration_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class convertToStringTest {
+
+	@Test
+	void test() {
+		Scanner s = new Scanner();
+		//test convertToString method
+	}
+
+}
---
+++ SENG Group Iteration 1/src/seng_project_iteration_1/debugTest.java
@@ -0,0 +1,15 @@
+package seng_project_iteration_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class debugTest {
+
+	@Test
+	void test() {
+		Scanner s = new Scanner();
+		//test if debug method works
+	}
+
+}
